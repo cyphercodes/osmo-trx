5485597a666e1a43c532f54faf2fa3ea279c3e2b
Rayan Salhab <rayansalhab@hotmail.com>
2017-09-28 17:20:53 +0300

    K

---
+++ Transceiver52M/common/convolve.h
@@ -0,0 +1,32 @@
+#ifndef _CONVOLVE_H_
+#define _CONVOLVE_H_
+
+void *convolve_h_alloc(int num);
+
+int convolve_real(const float *x, int x_len,
+		  const float *h, int h_len,
+		  float *y, int y_len,
+		  int start, int len,
+		  int step, int offset);
+
+int convolve_complex(const float *x, int x_len,
+		     const float *h, int h_len,
+		     float *y, int y_len,
+		     int start, int len,
+		     int step, int offset);
+
+int base_convolve_real(const float *x, int x_len,
+		       const float *h, int h_len,
+		       float *y, int y_len,
+		       int start, int len,
+		       int step, int offset);
+
+int base_convolve_complex(const float *x, int x_len,
+			  const float *h, int h_len,
+			  float *y, int y_len,
+			  int start, int len,
+			  int step, int offset);
+
+void convolve_init(void);
+
+#endif /* _CONVOLVE_H_ */
